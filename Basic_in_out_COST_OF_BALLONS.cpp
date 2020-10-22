f18348fba0b96b4eff20ab2ea520be84c9372808
Harsh.Trivedi <trivediharsh49@gmail.com>
2020-10-23 01:50:55 +0530

    Add files via upload

---
+++ Basic_in_out_COST_OF_BALLONS.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t,c,gbc,pbc,p,i=0,sum=0,a,b;
+	cin>>c;
+	for(i=0;i<c;i++)
+	{
+		if(i%2==0)
+		{
+				cin>>gbc;
+				cin>>pbc;
+				cin>>p;
+				for(t=0;t<p;t++)
+				{
+					cin>>a;
+					cin>>b;
+					if(a==1)
+					{
+						sum=sum+gbc;
+					}
+					if(b==1)
+					{
+						sum=sum+pbc;
+					}
+					a=0;
+					b=0;
+				}	
+		}
+		else
+		{
+			cin>>gbc;
+			cin>>pbc;
+			cin>>p;
+			t=gbc;
+			gbc=pbc;
+			pbc=t;
+
+			for(t=0;t<p;t++)
+			{
+				cin>>a;
+				cin>>b;
+				if(a==1)
+				{
+					sum=sum+gbc;
+				}
+				if(b==1)
+				{
+					sum=sum+pbc;
+				}
+				a=0;
+				b=0;
+			}	
+		}	
+		
+		cout<<sum<<"\n";
+		sum=0;
+
+	}
+	return 0;
+
+}
