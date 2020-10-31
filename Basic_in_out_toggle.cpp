e3b11054641a5941482619516e833b2392315d05
Harsh.Trivedi <trivediharsh49@gmail.com>
2020-11-01 02:04:24 +0530

    Update Basic_in_out_toggle.cpp

--- Basic_in_out_toggle.cpp
+++ Basic_in_out_toggle.cpp
@@ -1,3 +1,4 @@
+/* problem on = https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/modify-the-string/ */
 #include<bits/stdc++.h>
 #include<ctype.h>
 #include<string.h>
